3343a03081f1ae87bf21ea7701b19b326d1f75fb
Nicholas <nicholasnas.nn@gmail.com>
2025-08-28 14:56:56 -0300

    Ultima parte da atividade de numpy

--- src/c11/exercicios_lab/cap4_numpy/exercicio3.py
+++ src/c11/exercicios_lab/cap4_numpy/exercicio3.py
@@ -1,45 +1,68 @@
-# Exercicio 3 - Campo minado
+
 from random import randint
 import numpy as np
 
+# Atividade 3
 
-matriz = np.zeros((2,2))
+df = np.loadtxt('/home/nkk/Área de trabalho/projetos/C11/space.csv', delimiter=';', dtype=str, encoding='utf-8')
 
-number_linha = randint(0,1)
-number_coluna = randint(0,1)
 
-matriz[number_linha][number_coluna] = 1
-
-print('bomba na posição: ', number_linha, number_coluna)
-
-jogadas = 3
-acertos = 0
-
-while jogadas > 0:
-    print("Selecione um posição da matriz: ")
-    linha = int(input("Entre com a linha ( 0 ou 1): "))
-    coluna = int(input("Entre com a coluna ( 0 ou 1): "))
-    
-    if matriz[linha][coluna] == 0:
-        print("Correto acertou uma posição!".center(50,'-'))
-        matriz[linha][coluna] = 2 # local descoberto
+# 1 porcentagem de missos bem sucedidas
+def atividade1():
         
-        acertos += 1
-        if acertos == 3:
-            print("Congratulations! You beat the game!")
-            break
-        
-    elif matriz[linha][coluna] == 2:
-        print("posição já escolhida")
-        continue
+    status = df[1:,7]
+    total = len(status)
+    sucessos = np.sum(status == 'Success')
+    porcentagem = (sucessos/total) * 100
     
-    else:
-        print("Game Over! :( Try Again! ")
-        break   
-        
-    jogadas -= 1
-            
+    print(f"Porcentagens de missoes bem sucedidas: {porcentagem:.2f}%")
+
+def atividade2():
+    # Media de gastos por missao
+    custos = df[1:, 6]
+    custos = custos.astype(float)
+
+    custos_validos = custos[custos > 0]
+    media = np.mean(custos_validos)
+
+    print(f"Media de gastos por lancamento: {media:.2f}")
     
     
+def atividade3():
+    # Missoes lancadas pelo os EUA
+    local = df[1:, 2]
+    missoes_eua = np.sum(np.char.find(local, "USA") >= 0)
+
+    print(f"Quantidade de missoes lancadas nos USA: {missoes_eua}")
+
+
+def atividade4():
+    lacamentos = df[1:,1]
+    custos = df[1:,6]
+
+    spacex = custos[lacamentos == "SpaceX"].astype(float)
+    indice_maior_custo = np.argmax(spacex)
+    
+    missao = df[1:,:][lacamentos == "SpaceX"][indice_maior_custo]
+    print(f"Missao mais cara da spaceX {missao}")
+    
+def atividade5():
+    empresas = df[1:, 1]
+    unicas, contagens = np.unique(empresas, return_counts=True)
+
+    for empresa, contagens in zip(unicas, contagens):
+        print(f"{empresa}: {contagens} missões")
+
+
+if __name__ == "__main__":
+    atividade1()
+    atividade2()
+    atividade3()
+    atividade4()
+    atividade5()
+    
+    
+
+
     
     
